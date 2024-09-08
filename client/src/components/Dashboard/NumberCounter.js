a694495682837fe6f05c1598058b3db845ad2767
0ShNa0 <shravani053btcse21@igdtuw.ac.in>
2024-09-08 22:41:29 +0530

    dashboard construction

---
+++ client/src/components/Dashboard/NumberCounter.js
@@ -0,0 +1,30 @@
+import { Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+
+const NumberCounter = ({ endNumber, duration }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Calculate the increment value and interval time
+    const increment = endNumber / (duration / 100);
+    const interval = setInterval(() => {
+      setCount((prevCount) => {
+        const newCount = Math.ceil(prevCount + increment);
+        if (newCount >= endNumber) {
+          clearInterval(interval);
+          return endNumber;
+        }
+        return newCount;
+      });
+    }, 100); 
+    return () => clearInterval(interval);
+  }, [endNumber, duration]);
+
+  return (
+    <Typography style={{ textDecoration: 'none', color: '#212121', fontFamily: 'Playfair Display' ,fontSize:'22px'}}>
+      {count}
+    </Typography>
+  );
+};
+
+export default NumberCounter;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
+import { Typography } from '@mui/material';
+import '../App.css';
+import Typewriter from 'typewriter-effect';
 
 const Dashboard = () => {
   return (
-    <>
-   
+  <>
+<Typography
+  sx={{
+    fontSize: {
+      xs: '12px',  
+      sm: '30px', 
+      md: '40px',  
+      lg: '150px'  
+    },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    mt:{
+      xs: '6px',  
+      sm: '10px', 
+      md: '20px',  
+      lg: '30px'  
+    },
+ 
+    fontFamily:'Bebas Neue'
+  }}
+>
+    AlumniVerse
+   </Typography>
 
+           <Typography
+      sx={{
+        fontFamily: 'Lobster', 
+        fontSize: { xs: '20px', sm: '30px', md: '40px', lg: '50px' }, 
+        fontWeight: 'bold', 
+        textAlign: 'center', 
+        marginTop: '20px' 
+      }}
+    >
+      <Typewriter
+        options={{
+          strings: ['Your Network','Your World'],
+          autoStart: true,
+          loop: true,
+          delay: 75,
+          cursor: '_'
+        }}
+      />
+    </Typography>
    </>
   )
 }
