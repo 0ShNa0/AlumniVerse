609a82109a08dfa324eeae31692972e80aef7e75
Jiya-garg9971 <9971jiyagarg@gmail.com>
2024-09-09 16:43:12 +0530

    Add donation page

--- client/src/pages/Donation.jsx
+++ client/src/pages/Donation.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DonationCard from '../components/Donation/CardComponents.js';
+import DonationForm from '../components/Donation/DonationForm.js';
+
+import "../styles/Donation/main.css"
 
 const Donation = () => {
+  const [Form,showForm]=useState(false);
   return (
-    <div>Donation</div>
+    <div>
+      
+      <h1>Donate to a Cause</h1>
+      <DonationCard/>
+      <button className='apply-button' onClick={()=>showForm(true)}>
+        Apply for Donation
+      </button>
+      {Form && <DonationForm/>}
+
+    </div>
   )
 }
 
