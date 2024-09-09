e59902e25623fe2491b00f4c295a8bb06aa53f3e
manya chandna <manyachandna2122@gmail.com>
2024-09-09 23:34:05 +0530

    added photos to donation

--- client/src/utils/DonationData.js
+++ client/src/utils/DonationData.js
@@ -1,7 +1,7 @@
 const donationAreas = [
   {
     "id": 1,
-    "image": "https://via.placeholder.com/300x180", 
+    "image": "https://media.istockphoto.com/id/1333580948/photo/piggy-bank-with-graduation-cap-on-black-glass-floor-money-saving-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=vI1zgqVmIdeXPC6K_BsrmNRliabX_wj_EZ2jjsaaXvQ=",
     "title": "Scholarships for Underprivileged Students",
     "description": "Provide scholarships to talented students from low-income backgrounds to help them pursue their education without financial burden.",
     "goal": "100 scholarships by the end of the semester",
@@ -12,7 +12,7 @@ const donationAreas = [
   },
   {
     "id": 2,
-    "image": "https://via.placeholder.com/300x180",
+    "image": "https://media.istockphoto.com/id/1746645627/photo/side-view-of-young-male-scientist-looking-through-microscope-at-laboratory.webp?a=1&b=1&s=612x612&w=0&k=20&c=lUwssVi1olpt2U9EmVT74Fmc1Cstj5rrMYjoyc9S9N8=",
     "title": "Building a New Research Lab",
     "description": "Help fund the construction of a state-of-the-art research lab equipped with advanced technology for scientific innovation and exploration.",
     "goal": "Complete the lab construction by next academic year",
@@ -23,7 +23,7 @@ const donationAreas = [
   },
   {
     "id": 3,
-    "image": "https://via.placeholder.com/300x180",
+    "image": "https://media.istockphoto.com/id/2034780958/photo/stack-of-paper-with-mindfulness-text-and-a-brain-with-a-nature-view-in-the-human-head.webp?a=1&b=1&s=612x612&w=0&k=20&c=u0CWPLos5KpqkKRjTsgWof0NfSuv9EixPmoxHemId68=",
     "title": "Student Mental Health and Wellness Center",
     "description": "Support the establishment of a mental health and wellness center to provide counseling, workshops, and mental health resources for students.",
     "goal": "Set up 5 counseling rooms and 2 activity spaces",
@@ -34,7 +34,7 @@ const donationAreas = [
   },
   {
     "id": 4,
-    "image": "https://via.placeholder.com/300x180",
+    "image": "https://images.pexels.com/photos/9572726/pexels-photo-9572726.jpeg?auto=compress&cs=tinysrgb&w=600",
     "title": "Renovation of the College Library",
     "description": "Help renovate the college library to create a modern, tech-friendly learning environment with more digital resources and study spaces.",
     "goal": "Complete renovation by end of the semester",
@@ -45,7 +45,7 @@ const donationAreas = [
   },
   {
     "id": 5,
-    "image": "https://via.placeholder.com/300x180",
+    "image": "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8U3VwcG9ydCUyMGZvciUyMFN0dWRlbnQlMjBTdGFydHVwc3xlbnwwfHwwfHx8MA%3D%3D",
     "title": "Support for Student Startups",
     "description": "Provide funding and mentorship for student-led startups, fostering innovation and entrepreneurship within the college community.",
     "goal": "Fund 20 student startups this year",
@@ -56,7 +56,7 @@ const donationAreas = [
   },
   {
     "id": 6,
-    "image": "https://via.placeholder.com/300x180",
+    "image": "https://media.istockphoto.com/id/1249041775/photo/photo-depicting-the-person-who-focuses-on-the-target.webp?a=1&b=1&s=612x612&w=0&k=20&c=jznZlXe4_BoIHFKYzPhsGPemXS0nEBCe567MaAYKcj8=",
     "title": "Sports Facility Enhancement",
     "description": "Help upgrade the sports facilities, including new equipment, improved grounds, and professional coaching for various sports teams.",
     "goal": "Upgrade 3 sports grounds and purchase new equipment",
